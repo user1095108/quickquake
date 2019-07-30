5ecc47b501e84eb8a73fe0f64567aa658ef94212
Janez Zemva <janezz55@gmail.com>
2019-07-30 16:29:44 +0200

    some fixes

---
+++ glquake/sys_qt.c
@@ -0,0 +1,458 @@
+#include <sys/time.h>
+
+#include "quakedef.h"
+#include "errno.h"
+
+#define WARP_WIDTH              320
+#define WARP_HEIGHT             200
+
+/*
+================
+D_BeginDirectRect
+================
+*/
+void D_BeginDirectRect (int x, int y, byte *pbitmap, int width, int height)
+{
+}
+
+
+/*
+================
+D_EndDirectRect
+================
+*/
+void D_EndDirectRect (int x, int y, int width, int height)
+{
+}
+/*
+
+===============================================================================
+
+FILE IO
+
+===============================================================================
+*/
+
+#define MAX_HANDLES             10
+FILE    *sys_handles[MAX_HANDLES];
+
+int             findhandle (void)
+{
+	int             i;
+	
+	for (i=1 ; i<MAX_HANDLES ; i++)
+		if (!sys_handles[i])
+			return i;
+	Sys_Error ("out of handles");
+	return -1;
+}
+
+/*
+================
+filelength
+================
+*/
+int filelength (FILE *f)
+{
+	int             pos;
+	int             end;
+
+	pos = ftell (f);
+	fseek (f, 0, SEEK_END);
+	end = ftell (f);
+	fseek (f, pos, SEEK_SET);
+
+	return end;
+}
+
+int Sys_FileOpenRead (char *path, int *hndl)
+{
+	FILE    *f;
+	int             i;
+	
+	i = findhandle ();
+
+	f = fopen(path, "rb");
+	if (!f)
+	{
+		*hndl = -1;
+		return -1;
+	}
+	sys_handles[i] = f;
+	*hndl = i;
+	
+	return filelength(f);
+}
+
+int Sys_FileOpenWrite (char *path)
+{
+	FILE    *f;
+	int             i;
+	
+	i = findhandle ();
+
+	f = fopen(path, "wb");
+	if (!f)
+		Sys_Error ("Error opening %s: %s", path,strerror(errno));
+	sys_handles[i] = f;
+	
+	return i;
+}
+
+void Sys_FileClose (int handle)
+{
+	fclose (sys_handles[handle]);
+	sys_handles[handle] = NULL;
+}
+
+void Sys_FileSeek (int handle, int position)
+{
+	fseek (sys_handles[handle], position, SEEK_SET);
+}
+
+int Sys_FileRead (int handle, void *dest, int count)
+{
+	return fread (dest, 1, count, sys_handles[handle]);
+}
+
+int Sys_FileWrite (int handle, void *data, int count)
+{
+	return fwrite (data, 1, count, sys_handles[handle]);
+}
+
+int     Sys_FileTime (char *path)
+{
+	FILE    *f;
+	
+	f = fopen(path, "rb");
+	if (f)
+	{
+		fclose(f);
+		return 1;
+	}
+	
+	return -1;
+}
+
+void Sys_mkdir (char *path)
+{
+}
+
+
+/*
+===============================================================================
+
+SYSTEM IO
+
+===============================================================================
+*/
+
+void Sys_MakeCodeWriteable (unsigned long startaddr, unsigned long length)
+{
+}
+
+
+void Sys_Error (char *error, ...)
+{
+	va_list         argptr;
+
+	printf ("Sys_Error: ");   
+	va_start (argptr,error);
+	vprintf (error,argptr);
+	va_end (argptr);
+	printf ("\n");
+
+	exit (1);
+}
+
+void Sys_Printf (char *fmt, ...)
+{
+	va_list         argptr;
+	
+	va_start (argptr,fmt);
+	vprintf (fmt,argptr);
+	va_end (argptr);
+}
+
+void Sys_Quit (void)
+{
+	exit (0);
+}
+
+double Sys_FloatTime (void)
+{
+  struct timeval tp;
+  struct timezone tzp; 
+  static int      secbase; 
+
+  gettimeofday(&tp, &tzp);  
+
+  if (!secbase)
+  {
+    secbase = tp.tv_sec;
+    return tp.tv_usec/1000000.0;
+  }
+
+  return (tp.tv_sec - secbase) + tp.tv_usec/1000000.0;
+}
+
+char *Sys_ConsoleInput (void)
+{
+	return NULL;
+}
+
+void Sys_Sleep (void)
+{
+}
+
+void Sys_SendKeyEvents (void)
+{
+}
+
+void Sys_HighFPPrecision (void)
+{
+}
+
+void Sys_LowFPPrecision (void)
+{
+}
+
+//=============================================================================
+qboolean			isDedicated = false;
+
+char *basedir = ".";
+char *cachedir = "/tmp";
+
+cvar_t  sys_linerefresh = {"sys_linerefresh","0"};// set for entity display
+
+static double time, oldtime, newtime;
+
+void Sys_InitParms(int argc, char **argv)
+{
+	static quakeparms_t    parms;
+  int j;
+
+	memset(&parms, 0, sizeof(parms));
+
+	COM_InitArgv(argc, argv);
+	parms.argc = com_argc;
+	parms.argv = com_argv;
+
+	parms.memsize = 16*1024*1024;
+
+	j = COM_CheckParm("-mem");
+	if (j)
+		parms.memsize = (int) (Q_atof(com_argv[j+1]) * 1024 * 1024);
+	parms.membase = malloc (parms.memsize);
+
+	parms.basedir = basedir;
+// caching is disabled by default, use -cachedir to enable
+//	parms.cachedir = cachedir;
+
+	printf ("Host_Init\n");
+
+	Host_Init(&parms);
+
+  oldtime = Sys_FloatTime () - .1;
+}
+
+void Sys_RenderFrame()
+{
+	extern int vcrFile;
+	extern int recording;
+
+  newtime = Sys_FloatTime ();
+  time = newtime - oldtime;
+
+  if (cls.state == ca_dedicated)
+  {
+    if (time < sys_ticrate.value && (vcrFile == -1 || recording) )
+    {
+      return;
+    }
+
+    time = sys_ticrate.value;
+  }
+
+  if (time > sys_ticrate.value*2)
+    oldtime = newtime;
+  else
+    oldtime += time;
+
+  vid.recalc_refdef = true;
+  Host_Frame (time);
+}
+
+//int		texture_mode = GL_NEAREST;
+//int		texture_mode = GL_NEAREST_MIPMAP_NEAREST;
+//int		texture_mode = GL_NEAREST_MIPMAP_LINEAR;
+int		texture_mode = GL_LINEAR;
+//int		texture_mode = GL_LINEAR_MIPMAP_NEAREST;
+//int		texture_mode = GL_LINEAR_MIPMAP_LINEAR;
+
+int		texture_extension_number = 1;
+
+float		gldepthmin, gldepthmax;
+
+cvar_t	gl_ztrick = {"gl_ztrick","0"};
+
+const char *gl_vendor;
+const char *gl_renderer;
+const char *gl_version;
+const char *gl_extensions;
+
+qboolean is8bit = false;
+qboolean isPermedia = false;
+qboolean gl_mtexable = false;
+
+void GL_Init (void)
+{
+  gl_vendor = glGetString (GL_VENDOR);
+  Con_Printf ("GL_VENDOR: %s\n", gl_vendor);
+  gl_renderer = glGetString (GL_RENDERER);
+  Con_Printf ("GL_RENDERER: %s\n", gl_renderer);
+
+  gl_version = glGetString (GL_VERSION);
+  Con_Printf ("GL_VERSION: %s\n", gl_version);
+  gl_extensions = glGetString (GL_EXTENSIONS);
+//Con_Printf ("GL_EXTENSIONS: %s\n", gl_extensions);
+
+//	Con_Printf ("%s %s\n", gl_renderer, gl_version);
+
+  Cvar_SetValue ("gl_clear", 1.0);
+
+	glClearColor (1,0,0,0);
+	glCullFace(GL_FRONT);
+	glEnable(GL_TEXTURE_2D);
+
+	glEnable(GL_ALPHA_TEST);
+	glAlphaFunc(GL_GREATER, 0.666);
+
+	glPolygonMode (GL_FRONT_AND_BACK, GL_FILL);
+	glShadeModel (GL_FLAT);
+
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+//	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+}
+
+int scr_width;
+int scr_height;
+
+void GL_BeginRendering (int *x, int *y, int *width, int *height)
+{
+	*x = *y = 0;
+	*width = scr_width;
+	*height = scr_height;
+}
+
+void GL_EndRendering (void)
+{
+}
+
+void VID_Init(unsigned char *palette)
+{
+  int i;
+
+	Cvar_RegisterVariable (&gl_ztrick);
+
+	vid.maxwarpwidth = WARP_WIDTH;
+	vid.maxwarpheight = WARP_HEIGHT;
+	vid.colormap = host_colormap;
+	vid.fullbright = 256 - LittleLong (*((int *)vid.colormap + 2048));
+
+  GL_Init();
+
+	VID_SetPalette(palette);
+
+	vid.conwidth = vid.width = scr_width;
+	vid.conheight = vid.height = scr_height;
+
+	vid.aspect = ((float)vid.height / (float)vid.width) * (320.0 / 240.0);
+	vid.numpages = 2;
+
+  vid.recalc_refdef = 1;				// force a surface cache flush
+}
+
+unsigned short	d_8to16table[256];
+unsigned		d_8to24table[256];
+unsigned char	d_15to8table[65536];
+
+void	VID_Shutdown (void)
+{
+}
+
+void	VID_Update (vrect_t *rects)
+{
+}
+
+qboolean VID_Is8bit(void)
+{
+	return is8bit;
+}
+
+void VID_ShiftPalette(unsigned char *p)
+{
+//	VID_SetPalette(p);
+}
+
+void	VID_SetPalette (unsigned char *palette)
+{
+	byte	*pal;
+	unsigned r,g,b;
+	unsigned v;
+	int     r1,g1,b1;
+	int		j,k,l,m;
+	unsigned short i;
+	unsigned	*table;
+	FILE *f;
+	char s[255];
+	int dist, bestdist;
+
+//
+// 8 8 8 encoding
+//
+	pal = palette;
+	table = d_8to24table;
+	for (i=0 ; i<256 ; i++)
+	{
+		r = pal[0];
+		g = pal[1];
+		b = pal[2];
+		pal += 3;
+		
+		v = (255<<24) + (r<<0) + (g<<8) + (b<<16);
+		*table++ = v;
+	}
+	d_8to24table[255] &= 0xffffff;	// 255 is transparent
+
+	for (i=0; i < (1<<15); i++) {
+		/* Maps
+		000000000000000
+		000000000011111 = Red  = 0x1F
+		000001111100000 = Blue = 0x03E0
+		111110000000000 = Grn  = 0x7C00
+		*/
+		r = ((i & 0x1F) << 3)+4;
+		g = ((i & 0x03E0) >> 2)+4;
+		b = ((i & 0x7C00) >> 7)+4;
+		pal = (unsigned char *)d_8to24table;
+		for (v=0,k=0,bestdist=10000*10000; v<256; v++,pal+=4) {
+			r1 = (int)r - (int)pal[0];
+			g1 = (int)g - (int)pal[1];
+			b1 = (int)b - (int)pal[2];
+			dist = (r1*r1)+(g1*g1)+(b1*b1);
+			if (dist < bestdist) {
+				k=v;
+				bestdist = dist;
+			}
+		}
+		d_15to8table[i]=k;
+	}
+}
