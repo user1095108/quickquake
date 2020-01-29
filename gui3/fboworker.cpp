f0c4d5709c35f38320fcb457274a1943b10bd0bc
Janez Zemva <janezz55@gmail.com>
2020-01-29 11:29:44 +0100

    some fixes

--- gui3/fboworker.cpp
+++ gui3/fboworker.cpp
@@ -2,113 +2,23 @@
 
 #include "fboworker.hpp"
 
-class TextureNode : public QObject, public QSGSimpleTextureNode
-{
-  friend class FBOWorker;
-
-  Q_OBJECT
-
-  std::atomic<bool> workFinished_{};
-  unsigned char i_{};
-
-  struct
-  {
-    QScopedPointer<QOpenGLFramebufferObject> fbo;
-    QScopedPointer<QSGTexture> texture;
-
-    void reset()
-    {
-      fbo.reset();
-      texture.reset();
-    }
-  } fbo_[2];
-
-  QScopedPointer<QOpenGLContext> context_;
-  QOffscreenSurface surface_;
-
-  QQuickItem* item_;
-
-public:
-  explicit TextureNode(QQuickItem* const item) :
-    item_(item)
-  {
-    setFiltering(QSGTexture::Nearest);
-    setTextureCoordinatesTransform(QSGSimpleTextureNode::MirrorVertically);
-  }
-
-  ~TextureNode() noexcept
-  {
-    shutdown();
-  }
-
-  void shutdown() noexcept
-  {
-    workFinished_.store(false, std::memory_order_relaxed);
-
-    fbo_[0].reset();
-    fbo_[1].reset();
-
-    context_.reset();
-    surface_.destroy();
-  }
-
-  void suspend() noexcept
-  {
-    workFinished_.store(false, std::memory_order_relaxed);
-
-    fbo_[0].reset();
-    fbo_[1].reset();
-  }
-
-  Q_INVOKABLE void work()
-  {
-    context_->makeCurrent(&surface_);
-
-    auto& fbo(fbo_[i_ = (i_ + 1) % 2]);
-
-    auto const size((item_->size() *
-      item_->window()->effectiveDevicePixelRatio()).toSize());
-    Q_ASSERT(!size.isEmpty());
-
-    if (!fbo.fbo || (fbo.fbo->size() != size))
-    {
-      QOpenGLFramebufferObjectFormat format;
-      format.setAttachment(QOpenGLFramebufferObject::Depth);
-
-      fbo.fbo.reset(new QOpenGLFramebufferObject(size, format));
-      fbo.texture.reset(item_->window()->
-        createTextureFromId(fbo.fbo->texture(), size));
-    }
-
-    Q_ASSERT(fbo.fbo->isValid());
-    fbo.fbo->bind();
-
-    {
-      QQmlListReference const ref(item_, "resources");
-
-      for (int i{}, c(ref.count()); i != c; ++i)
-      {
-        QMetaObject::invokeMethod(ref.at(i), "render", Qt::DirectConnection,
-          Q_ARG(QSize, size));
-      }
-    }
-
-    workFinished_.store(true, std::memory_order_relaxed);
-
-    if (item_->isVisible())
-    {
-      context_->functions()->glFlush();
-
-      QMetaObject::invokeMethod(item_, "update", Qt::AutoConnection);
-    }
-  }
-};
-
 //////////////////////////////////////////////////////////////////////////////
 FBOWorker::FBOWorker(QQuickItem* const parent) :
   QQuickItem(parent)
 {
   setFlag(ItemHasContents);
+
+  connect(this, &QQuickItem::visibleChanged,
+    this,
+    [&]()
+    {
+      if (isVisible())
+      {
+        update();
+      }
+    },
+    Qt::DirectConnection
+  );
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -133,94 +43,104 @@ QSGNode* FBOWorker::updatePaintNode(QSGNode* const n,
   }
   else
   {
-    auto node(static_cast<TextureNode*>(n));
+    auto const v(isVisible());
 
-    if (auto const w(window()); !node)
+    auto const w(window());
+    Q_ASSERT(w);
+
+    auto node(static_cast<QSGSimpleTextureNode*>(n));
+
+    if (!node)
     {
-      Q_ASSERT(w);
-
-      node = new TextureNode(this);
-      node->moveToThread(QThread::currentThread());
+      node = new QSGSimpleTextureNode;
+      node->setFiltering(QSGTexture::Nearest);
+      node->setTextureCoordinatesTransform(
+        QSGSimpleTextureNode::MirrorVertically);
 
       node->setRect(br);
       node->setTexture(w->createTextureFromId(0, QSize()));
 
-      connect(this, &QQuickItem::visibleChanged,
-        node,
-        [this, node]()
-        {
-          if (isVisible())
-          {
-            QMetaObject::invokeMethod(node, "work", Qt::QueuedConnection);
-          }
-        },
-        Qt::DirectConnection
-      );
-
-      connect(w, &QQuickWindow::sceneGraphInitialized,
-        node, [&]() { update(); },
-        Qt::DirectConnection
-      );
-
-      connect(w, &QQuickWindow::sceneGraphInvalidated,
-        node, &TextureNode::suspend,
-        Qt::DirectConnection
-      );
-
-      Q_ASSERT(!node->context_);
-
-      auto const ccontext(w->openglContext());
-      Q_ASSERT(ccontext);
-
-      // this is done to safely share context resources
-      ccontext->doneCurrent();
-
-      auto f(ccontext->format());
-      f.setProfile(contextProfile_);
-
       {
-        node->context_.reset(new QOpenGLContext);
+        auto const ccontext(w->openglContext());
+        Q_ASSERT(ccontext);
 
-        auto& context(*node->context_);
-        context.moveToThread(QThread::currentThread());
+        // this is done to safely share context resources
+        ccontext->doneCurrent();
 
-        context.setFormat(f);
-        context.setShareContext(ccontext);
-        context.create();
-        Q_ASSERT(context.isValid());
+        auto f(ccontext->format());
+        f.setProfile(contextProfile_);
 
-        auto& surface(node->surface_);
+        {
+          context_.reset(new QOpenGLContext);
 
-        surface.setFormat(f);
-        surface.create();
-        Q_ASSERT(surface.isValid());
+          auto& context(*context_);
+          context.setFormat(f);
+          context.setShareContext(ccontext);
+          context.create();
+          Q_ASSERT(context.isValid());
+
+          surface_.setFormat(f);
+          surface_.create();
+          Q_ASSERT(surface_.isValid());
+        }
+
+        ccontext->makeCurrent(w);
       }
-
-      ccontext->makeCurrent(w);
-
-      connect(ccontext, &QOpenGLContext::aboutToBeDestroyed,
-        node, &TextureNode::suspend,
-        Qt::DirectConnection
-      );
-
-      QMetaObject::invokeMethod(node, "work", Qt::QueuedConnection);
     }
     else if (node->rect() != br)
     {
       node->setRect(br);
     }
-
-    if (node->workFinished_.load(std::memory_order_relaxed))
+    else if (v)
     {
-      node->workFinished_.store(false, std::memory_order_relaxed);
+      node->markDirty(QSGNode::DirtyMaterial);
+    }
 
-      node->setTexture(node->fbo_[node->i_].texture.get());
+    if (v)
+    {
+      auto const ccontext(w->openglContext());
+      context_->makeCurrent(&surface_);
 
-      QMetaObject::invokeMethod(node, "work", Qt::QueuedConnection);
+      auto const size((this->size() *
+        w->effectiveDevicePixelRatio()).toSize());
+      Q_ASSERT(!size.isEmpty());
+
+      if (!fbo_ || (fbo_->size() != size))
+      {
+        QOpenGLFramebufferObjectFormat format;
+        format.setAttachment(QOpenGLFramebufferObject::Depth);
+
+        fbo_.reset(new QOpenGLFramebufferObject(size, format));
+        texture_.reset(w->createTextureFromId(fbo_->texture(), size));
+
+        node->setTexture(texture_.get());
+
+        Q_ASSERT(fbo_->isValid());
+        fbo_->bind();
+      }
+
+      {
+        QQmlListReference const ref(this, "resources");
+
+        for (int i{}, c(ref.count()); i != c; ++i)
+        {
+          QMetaObject::invokeMethod(ref.at(i), "render", Qt::DirectConnection,
+            Q_ARG(QSize, size));
+        }
+      }
+
+      ccontext->makeCurrent(w);
+
+      update();
+    }
+    else
+    {
+      node->setTexture(w->createTextureFromId(0, QSize()));
+
+      fbo_.reset();
+      texture_.reset();
     }
 
     return node;
   }
 }
-
-#include "fboworker.moc"
--- gui3/fboworker.hpp
+++ gui3/fboworker.hpp
@@ -1,1 +1,40 @@
-../gui/fboworker.hpp
+#ifndef FBOWORKER_HPP
+# define FBOWORKER_HPP
+# pragma once
+
+class FBOWorker : public QQuickItem
+{
+  Q_OBJECT
+
+  Q_ENUMS(QSurfaceFormat::OpenGLContextProfile)
+
+  Q_PROPERTY(QSurfaceFormat::OpenGLContextProfile contextProfile READ contextProfile WRITE setContextProfile NOTIFY contextProfileChanged)
+
+  QScopedPointer<QOpenGLContext> context_;
+  QOffscreenSurface surface_;
+
+  QScopedPointer<QOpenGLFramebufferObject> fbo_;
+  QScopedPointer<QSGTexture> texture_;
+
+  QSurfaceFormat::OpenGLContextProfile contextProfile_{QSurfaceFormat::CompatibilityProfile};
+
+public:
+  explicit FBOWorker(QQuickItem* = nullptr);
+
+  auto contextProfile() const noexcept;
+  void setContextProfile(QSurfaceFormat::OpenGLContextProfile);
+
+private:
+  QSGNode* updatePaintNode(QSGNode*, QQuickItem::UpdatePaintNodeData*) final;
+
+signals:
+  void contextProfileChanged();
+};
+
+//////////////////////////////////////////////////////////////////////////////
+inline auto FBOWorker::contextProfile() const noexcept
+{
+  return contextProfile_;
+}
+
+#endif // FBOWORKER_HPP
