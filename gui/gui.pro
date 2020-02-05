b969ca18f5fa259eb2939015723d91c81a0dfdbb
Janez Zemva <janezz55@gmail.com>
2020-02-05 22:51:51 +0100

    some fixes

---
+++ gui/gui.pro
@@ -0,0 +1,20 @@
+!include(../common.pri) {
+  error("")
+}
+
+TEMPLATE = app
+
+HEADERS = $$files(*.hpp)
+SOURCES = $$files(*.cpp)
+RESOURCES = gui.qrc
+
+QT += qml quick
+
+CONFIG += precompile_header
+PRECOMPILED_HEADER  = "../precompile.hpp"
+
+*-g++* {
+  POST_TARGETDEPS += ../lib/libglquake.a
+}
+
+LIBS += -L ../lib -lglquake
---
+++ gui2/fboworker.hpp
@@ -0,0 +1,34 @@
+#ifndef FBOWORKER_HPP
+# define FBOWORKER_HPP
+# pragma once
+
+class FBOWorker : public QQuickItem
+{
+  Q_OBJECT
+
+  Q_ENUMS(QSurfaceFormat::OpenGLContextProfile)
+
+  Q_PROPERTY(QSurfaceFormat::OpenGLContextProfile contextProfile READ contextProfile WRITE setContextProfile NOTIFY contextProfileChanged)
+
+  QSurfaceFormat::OpenGLContextProfile contextProfile_{QSurfaceFormat::CompatibilityProfile};
+
+public:
+  explicit FBOWorker(QQuickItem* = nullptr);
+
+  auto contextProfile() const noexcept;
+  void setContextProfile(QSurfaceFormat::OpenGLContextProfile);
+
+private:
+  QSGNode* updatePaintNode(QSGNode*, QQuickItem::UpdatePaintNodeData*) final;
+
+signals:
+  void contextProfileChanged();
+};
+
+//////////////////////////////////////////////////////////////////////////////
+inline auto FBOWorker::contextProfile() const noexcept
+{
+  return contextProfile_;
+}
+
+#endif // FBOWORKER_HPP
